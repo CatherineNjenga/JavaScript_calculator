065a3d987aef9a3ffe3e970e18b842472bb197e1
Freshiah Catherine Njenga <87461018+CatherineNjenga@users.noreply.github.com>
2022-06-24 09:47:46 +0300

    "Initial Commit"

---
+++ calculator.js
@@ -0,0 +1,18 @@
+let answer = prompt("What operation would you like to make? ('+'/'*'/'/'/'%')")
+
+let numOne = Number(window.prompt("Kindly input your first number."))
+let numTwo = Number(window.prompt("Kindly input your second number."))
+
+if (answer == '+'){
+    result = numOne + numTwo;
+    alert(`The result is ${result}.`);
+} else if (answer == '*') {
+    result = numOne * numTwo;
+    alert(`The result is ${result}.`);
+} else if (answer == '/') {
+    result = numOne / numTwo;
+    alert(`The result is ${result}.`)
+} else if (answer == '%') {
+    result = numOne % numTwo;
+    alert(`The result is ${result}.`)
+}
