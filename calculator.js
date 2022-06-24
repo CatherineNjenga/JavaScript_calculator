5dafb89f80f72c423b54da8182823e700d46d7d9
Freshiah Catherine Njenga <87461018+CatherineNjenga@users.noreply.github.com>
2022-06-24 11:45:39 +0300

    "Including the subtraction operation"

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-let answer = prompt("What operation would you like to make? ('+'/'*'/'/'/'%')")
+let answer = prompt("What operation would you like to make? (input '+' or '*' or '-' or /' or '%')")
 
 let numOne = Number(window.prompt("Kindly input your first number."))
 let numTwo = Number(window.prompt("Kindly input your second number."))
@@ -9,6 +9,9 @@ if (answer == '+'){
 } else if (answer == '*') {
     result = numOne * numTwo;
     alert(`The result is ${result}.`);
+} else if (answer == '-') {
+    result = numOne - numTwo;
+    alert('The result is ${result}.')
 } else if (answer == '/') {
     result = numOne / numTwo;
     alert(`The result is ${result}.`)
